155523154fb8012fb89544f6b65e7e1f164a592d
codeanticode <andres.colubri@gmail.com>
2016-03-14 15:38:13 -0400

    added Android Studio project for Cardboard testing

---
+++ Android-Cardboard-Test/app/src/main/java/devel/android_cardboard_test/StereoSketch.java
@@ -0,0 +1,21 @@
+package devel.android_cardboard_test;
+
+import processing.cardboard.*;
+import processing.core.PApplet;
+
+public class StereoSketch extends PApplet {
+    public void settings() {
+        fullScreen(PGraphicsCardboard.RENDERER);
+    }
+
+    public void setup() {
+    }
+
+    public void draw() {
+        background(150);
+        translate(width/2, height/2);
+        rotateX(frameCount * 0.01f);
+        rotateY(frameCount * 0.01f);
+        box(200);
+    }
+}
