8fc1abb95aa50c80c73afd6723b1c9c5e1d69509
codeanticode <andres.colubri@gmail.com>
2016-03-22 18:01:17 -0400

    changed draw in fragment test

--- Android-Fragment-Test/src/devel/android_test/ProcessingSketch.java
+++ Android-Fragment-Test/src/devel/android_test/ProcessingSketch.java
@@ -13,22 +13,24 @@ public class ProcessingSketch extends PApplet {
   
   public void setup() {
     orientation(PORTRAIT);
-    fontA = loadFont("ArialMT-48.vlw"); //replace with any font created using create font tool
-    fill(color(0,0,0,255));
+    fontA = loadFont("ArialMT-48.vlw"); //replace with any font created using create font tool    
     textSize(48);    
   }
   
   public void draw() {
-    background(255,255,255);
-    if (millis()>delay) {
-      textFont(fontA, 48);
-    }
-    text("Watch the circles",80,80,width,100);
-    pushStyle();
-    fill(color(255,0,0,50));
-    ellipse(displayWidth*0.48f,displayHeight*0.5f,displayWidth*0.2f,displayWidth*0.2f);
+    background(255);
+//    if (millis()>delay) {
+//      textFont(fontA, 48);
+//    }
+    fill(color(0));
+    text("FPS " + frameRate,80,80);
+//    pushStyle();
+//    fill(color(255,0,0,50));
+//    ellipse(displayWidth*0.48f,displayHeight*0.5f,displayWidth*0.2f,displayWidth*0.2f);
+//    fill(color(0,0,255,50));
+//    ellipse(displayWidth*0.53f,displayHeight*0.5f,displayWidth*0.2f,displayWidth*0.2f);
+//    popStyle();
     fill(color(0,0,255,50));
-    ellipse(displayWidth*0.53f,displayHeight*0.5f,displayWidth*0.2f,displayWidth*0.2f);
-    popStyle();
+    ellipse(mouseX, mouseY, 100, 100);
   }  
 }
