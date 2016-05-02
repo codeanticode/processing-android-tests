491ad557f84334beb3d288bb17eb39e991f20dd7
codeanticode <andres.colubri@gmail.com>
2016-05-02 08:49:03 -0400

    watch faces with Android2D renderer are working

--- Android-Everything-Test/wear/src/main/java/devel/watchfacetest/WatchFaceSketch.java
+++ Android-Everything-Test/wear/src/main/java/devel/watchfacetest/WatchFaceSketch.java
@@ -13,23 +13,24 @@ public class WatchFaceSketch extends PApplet {
     public void setup() {
 //        background(60, 60, 160);
         frameRate(10);
+        fill(180, 150);
     }
 
     public void draw() {
 //        background(random(255), random(255), random(255));
 //        println("Processing draw:" + frameCount);
-        background(60, 60, 160);
-        fill(180, 150);
+        background(10, 200, 10);
+
 //        rect(random(0, width), random(0, height), 40, 40);
 //        line(0, 0, mouseX, mouseY);
 
 //        fill(200);
 
         translate(mouseX, mouseY);
-        rotate(angle);
+//        rotate(angle);
         rect(-25, 25, 50, 50);
         angle += 0.01;
 
-        println("FRAMECOUNT: " + frameCount);
+        println("FRAMECOUNT: " + frameCount + " res: " + width + "x" + height + " mouse: " + mouseX + ", " + mouseY);
     }
 }
