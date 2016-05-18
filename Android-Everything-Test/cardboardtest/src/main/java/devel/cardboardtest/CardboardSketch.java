2d7f7afe712fda8b0ba64b2a78dab80d8c5a9f85
codeanticode <andres.colubri@gmail.com>
2016-05-18 12:07:24 +0900

    updated cardboard test

--- Android-Everything-Test/cardboardtest/src/main/java/devel/cardboardtest/CardboardSketch.java
+++ Android-Everything-Test/cardboardtest/src/main/java/devel/cardboardtest/CardboardSketch.java
@@ -14,16 +14,16 @@ public class CardboardSketch extends PApplet {
     }
 
     public void draw() {
-      PGraphicsCardboard pgc = (PGraphicsCardboard)g;
-      if (pgc.eyeType == PCardboard.LEFT) {
-        background(200, 50, 50);
-      } else if (pgc.eyeType == PCardboard.RIGHT) {
-        background(50, 50, 200);
-      } else if (pgc.eyeType == PCardboard.MONOCULAR) {
-        background(50, 200, 50);
-      }
+//      PGraphicsCardboard pgc = (PGraphicsCardboard)g;
+//      if (pgc.eyeType == PCardboard.LEFT) {
+//        background(200, 50, 50);
+//      } else if (pgc.eyeType == PCardboard.RIGHT) {
+//        background(50, 50, 200);
+//      } else if (pgc.eyeType == PCardboard.MONOCULAR) {
+//        background(50, 200, 50);
+//      }
 
-//      background(157);
+      background(157);
       lights();
       translate(width / 2, height / 2);
 
