149488598dcc6e7f4ecf929a8f46b83de56b7cf8
codeanticode <andres.colubri@gmail.com>
2016-05-18 11:44:12 +0900

    add LGPL notices

--- Android-Everything-Test/libs/processing-cardboard/src/processing/cardboard/PSurfaceCardboard.java
+++ Android-Everything-Test/libs/processing-cardboard/src/processing/cardboard/PSurfaceCardboard.java
@@ -1,3 +1,25 @@
+/* -*- mode: java; c-basic-offset: 2; indent-tabs-mode: nil -*- */
+
+/*
+  Part of the Processing project - http://processing.org
+
+  Copyright (c) 2016 The Processing Foundation
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation, version 2.1.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General
+  Public License along with this library; if not, write to the
+  Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+  Boston, MA  02111-1307  USA
+*/
+
 package processing.cardboard;
 
 import java.io.File;
@@ -18,19 +40,15 @@ import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.content.pm.ConfigurationInfo;
 import android.content.res.AssetManager;
-import android.opengl.GLSurfaceView;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import processing.android.AppComponent;
-import processing.core.PApplet;
 import processing.core.PGraphics;
 import processing.opengl.PGLES;
 import processing.opengl.PGraphicsOpenGL;
 import processing.opengl.PSurfaceGLES;
 import android.view.Window;
 import android.view.WindowManager;
-import android.util.Log;
-import android.view.SurfaceView;
 
 public class PSurfaceCardboard extends PSurfaceGLES {
   protected GLCardboardSurfaceView glview;
@@ -113,7 +131,6 @@ public class PSurfaceCardboard extends PSurfaceGLES {
   }
 
   public void dispose() {
-    // TODO Auto-generated method stub
 //    surface.onDestroy();
   }
 
